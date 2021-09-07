547825aa6bd5de4fd6bbd86fe4d7ac6bea432417
saparatayev <sopa.97@mail.ru>
2021-09-07 09:32:46 +0500

    composition api

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,21 +6,28 @@
       <div class="grid grid-cols-12 gap-4">
         <div class="col-span-6 space-y-4 px-1"
              style="height: 500px">
-          <div class="p-8 bg-white shadow-md rounded flex items-center justify-between">
+          <div v-for="(todo, index) in todos"
+            :key="index"
+            class="p-8 bg-white shadow-md rounded flex items-center justify-between"
+            :class="{'bg-green-200': todo.done}"
+          >
             <div>
-              <div>sdcwubec</div>
-              <div class="text-gray-500 text-sm">Timestamp</div>
+              <div>{{ todo.text }}</div>
+              <div class="text-gray-500 text-sm">{{ todo.createdAt.toString() }}</div>
             </div>
             <div class="space-x-2">
               <button class="px-2 text-red-600"
+              @click="removeTodo(index)"
                       title="Remove todo">&times;</button>
-              <button class="px-2 text-green-600"
+              <button v-if="!todo.done" class="px-2 text-green-600"
+                      @click="markAsDone(index)"
                       title="Mark as done">&check;</button>
-              <button class="px-2 text-orange-600"
+              <button v-else class="px-2 text-orange-600"
+                      @click="markAsUndone(index)"
                       title="Mark as undone">&#8630;</button>
             </div>
           </div>
-          <div class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm">
+          <div v-if="todos.length === 0" class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm">
             You dont have any task to do.
           </div>
         </div>
@@ -28,10 +35,60 @@
         <div class="col-span-6">
           <div class="p-8 bg-white shadow-md rounded">
             <h2 class="text-xl">Add a todo</h2>
-            <input type="text" class="p-2 mt-4 border rounded w-full">
+            <!-- don't need todoText.value, value stands automatically -->
+            <input
+              v-model="todoText"
+              @keydown.enter="addTodo"
+              type="text" class="p-2 mt-4 border rounded w-full">
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { reactive } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const todos = reactive([])
+    const todoText = ref("") // const todoText = {value: ""}
+
+    function addTodo() {
+      todos.unshift({
+        text: todoText.value,
+        createdAt: new Date(),
+        done: false
+      })
+
+      todoText.value = ''
+    }
+
+    function markAsDone(index) {
+      todos[index].done = true
+    }
+    
+    function markAsUndone(index) {
+      todos[index].done = false
+    }
+
+    function removeTodo(index) {
+      if(!confirm('Are you sure?')) return;
+
+      todos.splice(index, 1)
+    }
+
+    return {
+      todos,
+      todoText,
+      addTodo,
+      markAsDone,
+      markAsUndone,
+      removeTodo
+    }
+  },
+})
+</script>
+
