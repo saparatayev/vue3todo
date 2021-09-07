63ef36978035cddc06cf78406bc607bae62b1f6a
saparatayev <sopa.97@mail.ru>
2021-09-07 13:04:48 +0500

    image upload - start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,55 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="mt-8 text-2xl">The Home Page</h1>
+  <div class="min-h-screen flex items-center justify-center">
+    <form class="p-8 w-1/2 bg-white rounded shadow">
+
+      <img v-show="imageUrl"
+           :src="imageUrl"
+           class="w-48 h-48 object-cover">
+      <div>
+        <input type="file" @change="onImageChange" accept="image/*">
+      </div>
+
+      <button class="mt-8 px-4 py-2 bg-indigo-700 text-white">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
+import { ref, watch } from 'vue';
 export default {
-  name: 'Home'
+  setup() {
+    let imageFile = ref("")
+    let imageUrl = ref("")
+
+    function onImageChange(e) {
+      let files = e.target.files || e.dataTransfer.files
+      
+      if (!files.length) {
+        imageUrl.value = ""
+        imageFile.value = ""
+        return
+      }
+
+      imageFile.value = files[0]
+    }
+
+    watch(imageFile, (imageFile) => {
+      if(!(imageFile instanceof File)) return
+
+      let reader = new FileReader()
+      
+      reader.readAsDataURL(imageFile)
+
+      reader.onload = () => {
+        imageUrl.value = reader.result
+        console.log(imageUrl.value)
+      };
+    })
+
+    return {
+      onImageChange,
+      imageUrl
+    }
+  },
 }
 </script>
